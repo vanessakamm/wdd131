e3c92da25c308a32120d093efd0b8f3df406698e
vanessakamm <kvanessa79@gmail.com>
2024-09-20 16:02:34 +0800

    ok

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,14 +2,12 @@
 const currentYear = new Date().getFullYear();
 
 const yearElement = document.getElementById('year');
-if (yearElement) {
-    yearElement.textContent = `©️${currentYear} 🌸Kam Yi Ching Vanessa🌸 Hong Kong SAR, China`;
+if (yearElement) {yearElement.textContent = `©️${currentYear} 🌸Kam Yi Ching Vanessa🌸 Hong Kong SAR, China`;
 }
 
-
-// Get the last modified date and add it to the second paragraph
 const lastModifiedElement = document.createElement('p');
 lastModifiedElement.textContent = `Last modified: ${document.lastModified}`;
+lastModifiedElement.classList.add('last-modified') //add in class to style in css//
 if (footer) {
     footer.appendChild(lastModifiedElement);
 }
