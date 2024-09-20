207c0f23ffac5ba1ff299c3dba071c6abb556ae8
vanessakamm <kvanessa79@gmail.com>
2024-09-20 16:28:08 +0800

    g

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,10 +1,18 @@
+
 const currentYear = new Date().getFullYear();
 
+// Update the year in the footer
 const yearElement = document.getElementById('year');
-if (yearElement) {yearElement.textContent = `©️${currentYear} 🌸Kam Yi Ching Vanessa🌸 Hong Kong SAR, China`;
+if (yearElement) {
+    yearElement.textContent = `©️${currentYear} 🌸Kam Yi Ching Vanessa🌸 Hong Kong SAR, China`;
 }
 
 const lastModifiedElement = document.createElement('p');
 lastModifiedElement.textContent = `Last modified: ${document.lastModified}`;
+
+
+const footer = document.querySelector('footer');
 if (footer) {
-footer.appendChild(lastModifiedElement);}
+   lastModifiedElement.classList.add('LastModifiedElement'); // Add class for styling
+    footer.appendChild(lastModifiedElement);
+}
